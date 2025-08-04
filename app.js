87e9da021ec50057e1739f1fdb75d1ddbac870df
Tejas Shinde <tejasshinde10@gmail.com>
2025-08-04 22:42:44 +0530

    update mongo URI

--- app.js
+++ app.js
@@ -88,7 +88,7 @@ app.use((req, res, next) => {
 
 app.use("/listings", listingsRouter);
 app.use("/listings/:id/reviews", reviewsRouter);
-// app.use("/", userRouter);
+app.use("/", userRouter);
 
 app.all("*", (req, res, next) => {
   next(new ExpressError(404, "Page Not Found!"));
